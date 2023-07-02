a17f780f0424fae685bc657c5b99c79d450127e4
Emmanuel Vargas <126733106+EmmanuelV22@users.noreply.github.com>
2023-07-02 18:12:24 +0000

    done

--- src/models.py
+++ src/models.py
@@ -7,26 +7,58 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    userName = Column(String(250), nullable=False)
+    firstName = Column(String(250), nullable=False)
+    lastName = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    address = Column(String(250), nullable=False)
+    phone = Column(String(250), nullable=False)
+    follower = relationship("follower")
 
-class Address(Base):
-    __tablename__ = 'address'
+class Follower(Base):
+    __tablename__ = 'follower'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    user_from_id = Column(Integer, ForeignKey('user.id'),primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'),primary_key=True)
+    def to_dict(self):
+        return {}
+    
+class Media(Base):
+    __tablename__ = 'media'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
+    type = Column(Integer)
+    url = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
     def to_dict(self):
         return {}
+    
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    def to_dict(self):
+        return {}
+    
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    def to_dict(self):
+        return {}    
 
 ## Draw from SQLAlchemy base
 try:
@@ -35,3 +67,6 @@ try:
 except Exception as e:
     print("There was a problem genering the diagram")
     raise e
+
+
+    
